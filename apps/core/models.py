1ea2285ecb4229f2a591e4a610300eaaf10975f0
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-10-10 17:49:04 -0300

    Enviando projeto

---
+++ apps/core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from apps.atracoes.models import Atracao
+from apps.comentarios.models import Comentario
+from apps.avaliacoes.models import Avaliacao
+from apps.enderecos.models import Endereco
+
+
+class PontoTuristico(models.Model):
+    nome = models.CharField(max_length=150)
+    descricao = models.TextField()
+    aprovado = models.BooleanField(default=False)
+    atracoes = models.ManyToManyField(Atracao)
+    comentario = models.ManyToManyField(Comentario)
+    avaliacoes = models.ManyToManyField(Avaliacao)
+    enderecos = models.ForeignKey(Endereco, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
