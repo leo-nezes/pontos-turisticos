5624b484be9b826a22e1ec1009cae4f53bf8f353
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-10-14 15:19:22 -0300

    [Pontos Turisticos] Seção 6:Filtros de querysets - Filtrando por query string

--- apps/core/src/viewsets.py
+++ apps/core/src/viewsets.py
@@ -7,4 +7,18 @@ class PontoTuristicoViewSet(ModelViewSet):
     serializer_class = PontoTuristicoSerializer
 
     def get_queryset(self):
-        return PontoTuristico.objects.filter(aprovado=True)
+        id = self.request.query_params.get('id', None)
+        nome = self.request.query_params.get('nome', None)
+        descricao = self.request.query_params.get('descricao', None)
+        query_set = PontoTuristico.objects.all()
+
+        if id:
+            query_set = PontoTuristico.objects.filter(pk=id) # ou (id=id)
+
+        if nome:
+            query_set = query_set.filter(nome__iexact=nome)
+
+        if descricao:
+            query_set = query_set.filter(descricao__iexact=descricao)
+
+        return query_set
